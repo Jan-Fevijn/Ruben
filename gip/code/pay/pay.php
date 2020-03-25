1e9b1b13911fed4d37b566992db7000c6209a2bf
Ruben <ruben.aspeslag@gmail.com>
2020-03-25 13:56:26 +0100

    gebruiksaanwijzing gewijzigd (gip)

--- gip/code/pay/pay.php
+++ gip/code/pay/pay.php
@@ -6,13 +6,13 @@
     <title></title>
     <link rel="stylesheet"
           type="text/css"
-          href="../../../css/css.css" />
+          href="../../css/css.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 <div id="banner">
         <h1>arduino educatief</h1><br />
-        <a href="../../index.php" id="bannerBut">home</a>
+        <a href="../index.php" id="bannerBut">home</a>
         <a href="../ourService.php" id="bannerBut">onze service</a>
         <a href="../aboutUs.php" id="bannerBut">over ons</a>
         <a href="../contactUs.php" id="bannerBut">contacteer ons</a>
@@ -33,12 +33,13 @@
  paypall : 
  </td>
  <td>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+ <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="DZ2Y8NFBBS54G">
-<input type="image" src="https://www.paypalobjects.com/nl_NL/BE/i/btn/btn_buynowCC_LG.gif" border="0" width="200" height="100" name="submit" alt="PayPal, de veilige en complete manier van online betalen.">
-<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" >
+<input type="image" src="https://www.paypalobjects.com/nl_NL/BE/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, de veilige en complete manier van online betalen.">
+<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 </td>
 <td>
 betaal veilig en vlot met paypal , het maakt niet uit waar u shop, u bankgegevens worden nooit met we webshop gedeeld.
